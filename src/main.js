255588c82ad9b9c316927075e35cadfc551ec691
Julio <jrab997@gmail.com>
2022-05-13 07:35:15 -0500

    first commir

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const d = document;
+
+const loadDayQuote = async () => {
+  try {
+    let res = await fetch('https://api.quotable.io/random');
+    if (!res.ok) return new Error(res.statusText);
+    let quote = await res.json();
+    console.log(quote);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const renderQuote = () => {
+  const $quote = d.getElementById('quote');
+  const $authot = d.getElementById('author');
+};
+d.addEventListener('DOMContentLoaded', loadDayQuote);
